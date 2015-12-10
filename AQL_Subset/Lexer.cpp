b6b633079293aee01fcc945194160dc8224f5f14
laiy <ly.franky@gmail.com>
2015-12-10 20:44:57 +0800

    lexer done

---
+++ AQL_Subset/Lexer.cpp
@@ -0,0 +1,81 @@
+#include <vector>
+#include <string>
+#include <functional>
+#include "Lexer.h"
+#include "Tokenizer.h"
+
+inline token get_token(std::string);
+inline bool is_number(const std::string& s);
+
+Lexer::Lexer(char *file_path) {
+    Tokenizer tokenizer = Tokenizer(file_path);
+    std::vector<std::string> tokens = tokenizer.get_tokens();
+    for (int i = 0; (size_t)i < tokens.size(); i++)
+        this->tokens.push_back(get_token(tokens[i]));
+}
+
+std::vector<token> Lexer::get_tokens() {
+    return this->tokens;
+}
+
+inline token get_token(std::string str) {
+    Type type;
+    if (str == ".")
+        type = DOT;
+    else if (str == "<")
+        type = LESSTHAN;
+    else if (str == ">")
+        type = GREATERTHAN;
+    else if (str == "(")
+        type = LEFTBRACKET;
+    else if (str == ")")
+        type = RIGHTBRACKET;
+    else if (str == "{")
+        type = CURLYLEFTBRACKET;
+    else if (str == "}")
+        type = CURLYRIGHTBRACKET;
+    else if (str == ";")
+        type = SEMICOLON;
+    else if (str == "create")
+        type = CREATE;
+    else if (str == "view")
+        type = VIEW;
+    else if (str == "as")
+        type = AS;
+    else if (str == "output")
+        type = OUTPUT;
+    else if (str == "select")
+        type = SELECT;
+    else if (str == "from")
+        type = FROM;
+    else if (str == "extract")
+        type = EXTRACT;
+    else if (str == "regex")
+        type = REGEX;
+    else if (str == "on")
+        type = ON;
+    else if (str == "return")
+        type = RETURN;
+    else if (str == "group")
+        type = GROUP;
+    else if (str == "and")
+        type = AND;
+    else if (str == "Token")
+        type = TOKEN;
+    else if (str == "pattern")
+        type = PATTERN;
+    else if (is_number(str))
+        type = NUM;
+    else if (str[0] == '/' && str[str.length() - 1] == '/')
+        type = REG;
+    else
+        type = ID;
+    return token(str, type);
+}
+
+inline bool is_number(const std::string& s) {
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
+
---
+++ AQL_Subset/Lexer.h
@@ -0,0 +1,26 @@
+#include <vector>
+#include <string>
+
+typedef enum {
+    CREATE, VIEW, AS, OUTPUT, SELECT, FROM, EXTRACT, REGEX, ON, RETURN,
+    GROUP, AND, TOKEN, PATTERN, ID, DOT, REG, NUM, LESSTHAN, GREATERTHAN,
+    LEFTBRACKET, RIGHTBRACKET, CURLYLEFTBRACKET, CURLYRIGHTBRACKET, SEMICOLON
+} Type;
+
+struct token {
+    std::string value;
+    Type type;
+    token(std::string value, Type type) {
+        this->value = value;
+        this->type = type;
+    }
+};
+
+class Lexer {
+    public:
+        Lexer(char *file_path);
+        std::vector<token> get_tokens();
+    private:
+        std::vector<token> tokens;
+};
+
